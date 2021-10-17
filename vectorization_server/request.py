f576a063f02bde25d6550b5b4f58722e82018b22
Ashwani Rathee <ab669522@gmail.com>
2021-10-17 12:31:15 +0530

    rater to vector tool

---
+++ vectorization_server/request.py
@@ -0,0 +1,15 @@
+import requests
+from skimage import io
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# resp = requests.post("http://localhost:3000/tosvg",
+#                      files={"file":open('temp.png','rb')})
+
+resp = requests.post("http://localhost:3000/tosvg",
+                     files={"file":open('temp.png','rb')})
+json_load= resp.json()
+a_restored = np.asarray(json_load["output"])
+print(a_restored)
